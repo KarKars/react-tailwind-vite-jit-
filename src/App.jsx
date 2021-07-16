03dd966ec2521a487e44ac33647231c6fe73882e
KarKars <karim.yah2024@gmail.com>
2021-07-16 12:06:44 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <h1 className="text-black text-3xl">Hello </h1>
+    </div>
+  );
+}
+
+export default App;
